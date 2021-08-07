f6047904e7a7016a744d3ffa5d157ad6c943c227
bambuco2 <domen.dobnikar@gmail.com>
2021-08-07 12:53:06 +0200

    adding login logic

--- webstore/views/views.py
+++ webstore/views/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from webstore.models import User
 
 # Create your views here.
 
@@ -6,4 +7,13 @@ def home(request):
     return render(request, "webstore/home.html")
 
 def login(request):
-    return render(request, "webstore/login.html")
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        if username and password:
+            exists = (User.objects.filter(userName = username).exists() and User.objects.filter(password = password).exists())
+            if exists:
+                return render(request, "webstore/home.html")
+        return render(request, "webstore/login.html")
+    else:
+        return render(request, "webstore/login.html")
