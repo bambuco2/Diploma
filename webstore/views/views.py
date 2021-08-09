1efc2db1a8dd57fcd583c282d333648616cf70c9
bambuco2 <domen.dobnikar@gmail.com>
2021-08-09 12:43:14 +0200

    Create session logic Fixes #4 Adding session logic

--- webstore/views/views.py
+++ webstore/views/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import redirect, render
 from webstore.models import User
 
 # Create your views here.
+loggedUser = None
+class LoggedUser:
+    logged = False
+    def __init__(self, userID, userName, password, name, surname):
+        self.userID = userID
+        self.userName = userName
+        self.password = password
+        self.name = name
+        self.surname = surname
+        self.logged = True
 
 def home(request):
+    if(loggedUser is not None and loggedUser.logged):
+        return render(request, "webstore/homeLogged.html")
     return render(request, "webstore/home.html")
 
 def login(request):
@@ -11,9 +23,14 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
         if username and password:
-            exists = (User.objects.filter(userName = username).exists() and User.objects.filter(password = password).exists())
-            if exists:
-                return render(request, "webstore/home.html")
+            user = (User.objects.filter(userName = username, password = password))
+            if user:
+                global loggedUser
+                loggedUser = LoggedUser(user[0].userID, user[0].userName, user[0].password, user[0].name, user[0].surname)
+                return redirect("home")
         return render(request, "webstore/login.html")
     else:
         return render(request, "webstore/login.html")
+
+def logout(request):
+    return render(request, "webstore/login.html")
